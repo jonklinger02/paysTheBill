97d582ac199b17db990c2e5d231370eb735c7178
jonklinger02 <jon.klinger02@gmail.com>
2022-01-08 05:27:08 -0500

    remove unecessary files

---
+++ src/components/stage_1.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+const Stage1 = () => {
+  return (
+    <>
+      Stage1
+    </>
+  );
+}
+
+export default Stage1;
---
+++ src/components/stage_2.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+const Stage2 = () => {
+  return (
+    <>
+      Stage2
+    </>
+  );
+}
+
+export default Stage2;
