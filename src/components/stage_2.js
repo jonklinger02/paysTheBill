b29f79cdfc75cc8b0e35a622e2bac0ab19191205
jonklinger02 <jon.klinger02@gmail.com>
2022-01-08 10:26:52 -0500

    update to working app, finito

--- src/components/stage_2.js
+++ src/components/stage_2.js
@@ -1,12 +1,27 @@
-import React from "react";
+import React, {useContext} from "react";
+import { MyContext } from "../context";
+
 
 
 const Stage2 = () => {
-  return (
-    <>
-      Stage2
-    </>
-  );
+    const context = useContext(MyContext);
+
+    return (
+        <>
+            <div className="result_wrapper">
+                <h3>The loser is: </h3>
+                <div>{context.state.result}</div>
+            </div>
+            <div className='action_button'
+            onClick={context.startOver}>
+                START OVER
+            </div>
+            <div className='action_button btn_2'
+            onClick={context.newLoser}>
+                GET NEW LOSER
+            </div>
+        </>
+    );
 }
 
 export default Stage2;
--- src/context/index.js
+++ src/context/index.js
@@ -1,4 +1,5 @@
 import React, {Component} from "react";
+import { ToastContainer, toast } from "react-toastify";
 
 
 const MyContext = React.createContext();
@@ -11,19 +12,81 @@ class MyProvider extends Component {
         result:''
     }
 
+    addPlayerHandler = (name) => {
+        this.setState((prevState)=>({
+            players:[ ...prevState.players,
+                name
+            ]
+        }))
+    }
+
+    removePlayerHandler = (idx) => {
+
+        let newArray = this.state.players;
+        newArray.splice(idx,1);
+        this.setState({players:newArray})
+       
+   }
+
+    nextHandler = () => {
+
+     const { players } = this.state
+
+     if (players.length < 2 ){
+        // toast
+        toast.error('You should add more than one player',
+        {
+            position: toast.POSITION.TOP_CENTER,
+            autoClose: 2000
+        })
+     } else {
+         //show next
+         this.setState({stage:2}, ()=> {
+             setTimeout(()=> {
+                 this.generateLoser()
+             },1500)
+         })
+     }
+    
+    }
+
+    generateLoser = () => {
+        const {players} = this.state
+        this.setState({
+            result: players[Math.floor(Math.random() * players.length)]
+        })
+    }
+
+    startOverHandler = () => {
+        this.setState({
+            stage:1,
+            players: [],
+            result: ''
+        })
+    }
+
     render() {
         return(
-            <MyContext.Provider value={{
-                state: this.state
-            }}>
-                {this.props.children}
-            </MyContext.Provider>
+            <>
+                <MyContext.Provider value={{
+                    state: this.state,
+                    addPlayer: this.addPlayerHandler,
+                    removePlayer: this.removePlayerHandler,
+                    next: this.nextHandler,
+                    newLoser: this.generateLoser,
+                    startOver: this.startOverHandler
+                }}>
+                    {this.props.children}
+                </MyContext.Provider>
+                <ToastContainer />
+            </>
 
         )
     }
 }
 
 export {
+
     MyContext,
     MyProvider
     
